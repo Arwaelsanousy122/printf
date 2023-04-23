cb5aed62fea0d59f4f7a2872b0c91c0201ed5fba
Arwaelsanousy122 <yehiasaad2009@gmail.com>
2023-04-23 05:43:13 -0700

    Some initial steps to try checker

---
+++ 0_printf.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+}
+
+/**
+ * _printf - building ower own printf function.
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings).
+ */
+
+int main(void)
+{
+
+}
