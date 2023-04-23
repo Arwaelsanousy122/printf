c2abfc3db41b8685f82f54d8f181867008621127
Arwaelsanousy122 <yehiasaad2009@gmail.com>
2023-04-23 06:41:37 -0700

    Task 0 first reel try

--- 0_printf.c
+++ 0_printf.c
@@ -1,10 +1,7 @@
 #include <stdarg.h>
-
-int _printf(const char *format, ...)
-{
-	va_list args;
-	va_start(args, format);
-}
+#include "main.h"
+#include <stdout.h>
+#include <stdio.h>
 
 /**
  * _printf - building ower own printf function.
@@ -13,7 +10,38 @@ int _printf(const char *format, ...)
  * (excluding the null byte used to end output to strings).
  */
 
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	for (*format != '\0')
+	{
+		if (*format == '%')
+		{
+		format++;
+		switch (*format)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				printf("%s", va_arg(args, char*));
+				break;
+		}
+		else
+		{
+			printf("%c", *format);
+		}
+		format++;
+		}
+	}
+	va_end(args);
+}
+
 int main(void)
 {
-
+	char c = 'A';
+	char str[] = "Hello world!";
+        _printf("character: %c\n, String: %s\n", ,);
+        return (0);
 }
